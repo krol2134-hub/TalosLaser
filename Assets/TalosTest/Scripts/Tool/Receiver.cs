2e049edc2633149ad0aaf81bac8b23443483a97e
krol2134-hub <nikita1995f@gmail.com>
2025-06-22 23:46:09 +0300

    Add Activate/Deactivate color state for Receiver with Visual effect;

--- Assets/TalosTest/Scripts/Tool/Receiver.cs
+++ Assets/TalosTest/Scripts/Tool/Receiver.cs
@@ -1,10 +1,41 @@
-﻿namespace TalosTest.Tool
+﻿using System;
+using UnityEngine;
+
+namespace TalosTest.Tool
 {
     public class Receiver : LaserInteractable
     {
+        [SerializeField] private Color targetColor = Color.red;
+        [SerializeField] private GameObject activateEffect;
+        
+        public bool IsActivate { get; private set; }
+
+        private void Awake()
+        {
+            activateEffect.SetActive(IsActivate);
+        }
+
         public override bool CanConnectLaser()
         {
             return true;
+        }
+
+        public override void AddInputLaser(Color color)
+        {
+            base.AddInputLaser(color);
+
+            UpdateActivationState();
+        }
+        
+        private void UpdateActivationState()
+        {
+            var isActivate = InputLaserColors.Contains(targetColor);
+            if (IsActivate != isActivate)
+            {
+                IsActivate = isActivate;
+
+                activateEffect.SetActive(isActivate);
+            }
         }
     }
 }
