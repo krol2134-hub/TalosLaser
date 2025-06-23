91bdcc109541a2602db0fe6e9b1f79439930c10f
krol2134-hub <nikita1995f@gmail.com>
2025-06-23 09:52:54 +0300

    Display LaserPath effect between connections;
    Code clean up in LaserPath;

--- Assets/TalosTest/Scripts/Tool/Connector.cs
+++ Assets/TalosTest/Scripts/Tool/Connector.cs
@@ -1,14 +1,22 @@
-﻿using TalosTest.Character;
+﻿using System.Collections.Generic;
+using TalosTest.Character;
+using TalosTest.Visuals;
+using UnityEngine;
 
 namespace TalosTest.Tool
 {
     public class Connector : MovableTool
     {
+        [SerializeField] private LaserPathEffect laserPathEffectPrefab;
+        
+        private readonly Dictionary<LaserInteractable, LaserPathEffect> _spawnedLaserPathEffects = new();
+        
         public override void PickUp(Interactor interactor)
         {
             base.PickUp(interactor);
             
             ClearConnections();
+            DespawnLasers();
         }
 
         public override void Drop(Interactor interactor)
@@ -18,6 +26,40 @@ namespace TalosTest.Tool
             foreach (var laserInteractable in interactor.HeldConnections)
             {
                 AddOutputConnection(laserInteractable);
+                DisplayLaserPath(laserInteractable);
+            }
+        }
+
+        private void DisplayLaserPath(LaserInteractable targetInteractable)
+        {
+            var targetLaserPosition = targetInteractable.LaserPoint;
+            var laserDirection = (targetLaserPosition - LaserPoint);
+                
+            var laserPathEffect = Instantiate(laserPathEffectPrefab, LaserPoint, Quaternion.identity);
+            laserPathEffect.SetPath(laserDirection.normalized, laserDirection.magnitude);
+                
+            _spawnedLaserPathEffects.Add(targetInteractable, laserPathEffect);
+        }
+
+        private void DespawnLasers()
+        {
+            foreach (var laserPathEffect in _spawnedLaserPathEffects.Values)
+            {
+                //TODO Use Pool
+                Destroy(laserPathEffect.gameObject);
+            }
+            
+            _spawnedLaserPathEffects.Clear();
+        }
+
+        protected override void RemoveOutputConnection(LaserInteractable inputInteractable)
+        {
+            base.RemoveOutputConnection(inputInteractable);
+
+            if (_spawnedLaserPathEffects.TryGetValue(inputInteractable, out var laserPathEffect))
+            {
+                Destroy(laserPathEffect.gameObject);
+                _spawnedLaserPathEffects.Remove(inputInteractable);
             }
         }
 
---
+++ Assets/TalosTest/Scripts/Visuals/LaserPathEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace TalosTest.Visuals
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class LaserPathEffect : MonoBehaviour
+    {
+        [SerializeField] private float defaultLength = 30f;
+        
+        private LineRenderer _lineRenderer;
+
+        private Vector3 _endPosition;
+        private float _length;
+        
+        private void Awake()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+            
+            SetPath(transform.forward, defaultLength);
+        }
+
+        public void SetPath(Vector3 direction, float length)
+        {
+            _length = length;
+            _endPosition = transform.position + direction * _length;
+            
+            _lineRenderer.SetPosition(0, transform.position);
+            _lineRenderer.SetPosition(1, _endPosition);
+            _lineRenderer.textureScale = new Vector2(_length, 1f);
+        }
+    }
+}
