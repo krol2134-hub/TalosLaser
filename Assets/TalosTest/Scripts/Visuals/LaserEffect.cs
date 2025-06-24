0d59e45bfbfb93084029efb7ac17d77f4cd4be07
krol2134-hub <nikita1995f@gmail.com>
2025-06-25 01:15:44 +0300

    Added Physics Laser blockers by Player and Env;

--- Assets/TalosTest/Scripts/Visuals/LaserEffect.cs
+++ Assets/TalosTest/Scripts/Visuals/LaserEffect.cs
@@ -1,10 +1,13 @@
-﻿using TalosTest.Tool;
+﻿using System;
+using TalosTest.Tool;
 using UnityEngine;
 
 namespace TalosTest.Visuals
 {
     // based on Hovl Studio laser script
     [RequireComponent(typeof(LineRenderer))]
+    [RequireComponent(typeof(MeshCollider))]
+    //[ExecuteInEditMode]
     public class LaserEffect : MonoBehaviour
     {
         [SerializeField] private float maxLength = 30;
@@ -14,6 +17,9 @@ namespace TalosTest.Visuals
         private static readonly int MainTex = Shader.PropertyToID("_MainTex");
         private static readonly int Noise = Shader.PropertyToID("_Noise");
 
+        private MeshCollider _meshCollider;
+        
+        private Mesh _lastMesh;
         private Vector4 _length = new(1, 1, 1, 1);
 
         public LineRenderer LineRenderer { get; private set; }
@@ -23,12 +29,18 @@ namespace TalosTest.Visuals
         private void Awake()
         {
             LineRenderer = GetComponent<LineRenderer>();
+            _meshCollider = GetComponent<MeshCollider>();
+        }
+
+        private void OnDestroy()
+        {
+            ClearMesh();
         }
 
         private void Start()
         {
-            LineRenderer.material.SetTextureScale(MainTex, new Vector2(_length[0], _length[1]));
-            LineRenderer.material.SetTextureScale(Noise, new Vector2(_length[2], _length[3]));
+            LineRenderer.sharedMaterial.SetTextureScale(MainTex, new Vector2(_length[0], _length[1]));
+            LineRenderer.sharedMaterial.SetTextureScale(Noise, new Vector2(_length[2], _length[3]));
         }
 
         public void Setup(ColorType colorType)
@@ -46,6 +58,36 @@ namespace TalosTest.Visuals
         {
             LineRenderer.positionCount++;
             LineRenderer.SetPosition(LineRenderer.positionCount - 1, point);
+
+            BakeCollider();
+        }
+
+        [ContextMenu("BakeCollider")]
+        private void BakeCollider()
+        {
+            ClearMesh();
+            
+            /*var mesh = new Mesh();
+            LineRenderer.BakeMesh(mesh);
+            _meshCollider.sharedMesh = mesh;
+            _lastMesh = mesh;*/
+        }
+
+        private void ClearMesh()
+        {
+            if (_lastMesh == null)
+            {
+                return;
+            }
+            
+            if (Application.isPlaying)
+            {
+                Destroy(_lastMesh);
+            }
+            else
+            {
+                DestroyImmediate(_lastMesh);
+            }
         }
     }
 }
