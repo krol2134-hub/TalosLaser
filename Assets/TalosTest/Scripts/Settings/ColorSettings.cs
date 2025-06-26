c49e1daf4f559cc6a5db49ad61ef92a4f0e31320
krol2134-hub <nikita1995f@gmail.com>
2025-06-26 12:49:20 +0300

    Added ColorsSettings SO;
    Create Settings folders for contain Setting script and SO;

---
+++ Assets/TalosTest/Scripts/Settings/ColorSettings.cs
@@ -0,0 +1,24 @@
+using TalosTest.Tool;
+using UnityEngine;
+
+namespace TalosTest.Settings
+{
+    [CreateAssetMenu(fileName = "ColorSettings", menuName = "TalosTest/Settings/ColorSettings", order = 1)]
+    public class ColorSettings : ScriptableObject
+    {
+        [SerializeField] private ColorSettingsByType[] colorSettingsByTypes;
+
+        public ColorSettingsByType GetColorSettingsByType(ColorType colorType)
+        {
+            foreach (var colorSettings in colorSettingsByTypes)
+            {
+                if (colorSettings.ColorType == colorType)
+                {
+                    return colorSettings;
+                }
+            }
+
+            return default;
+        }
+    }
+}
---
+++ Assets/TalosTest/Scripts/Settings/ColorSettingsByType.cs
@@ -0,0 +1,18 @@
+using System;
+using TalosTest.Tool;
+using UnityEngine;
+
+namespace TalosTest.Settings
+{
+    [Serializable]
+    public struct ColorSettingsByType
+    {
+        [SerializeField] private ColorType colorType;
+        [SerializeField] private Color laserSphereMainColor;
+        [SerializeField] private Color laserSphereAdditionalColor;
+
+        public ColorType ColorType => colorType;
+        public Color LaserSphereMainColor => laserSphereMainColor;
+        public Color LaserSphereAdditionalColor => laserSphereAdditionalColor;
+    }
+}
