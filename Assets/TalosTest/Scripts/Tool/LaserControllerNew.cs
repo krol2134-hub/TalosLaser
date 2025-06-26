287950002925eab92bf1d685a2163cae0f08f6a1
krol2134-hub <nikita1995f@gmail.com>
2025-06-26 17:32:51 +0300

    Added blocks mechanic between independed lines;
    Create MathematicsHelper for contain Intersept logic;

--- Assets/TalosTest/Scripts/Tool/LaserControllerNew.cs
+++ Assets/TalosTest/Scripts/Tool/LaserControllerNew.cs
@@ -8,6 +8,7 @@ namespace TalosTest.Tool
     {
         [SerializeField] private LayerMask obstacleMask;
         [SerializeField] private LaserVfxController laserVFXController;
+
 #if UNITY_EDITOR
         [SerializeField] private int pathCountDebug;
         [SerializeField, Range(-1, 10)] private int maxPathCountDebug = -1;
@@ -20,9 +21,11 @@ namespace TalosTest.Tool
 
         private readonly HashSet<LaserInteractable> _previousFrameInteractables = new();
         private readonly HashSet<LaserInteractable> _currentFrameInteractables = new();
-        
+
         private LaserPathGenerator _laserPathGenerator;
-        
+
+        public Dictionary<Generator, List<List<LaserSegment>>> _segmentsbyGenerator = new();
+
         private void Awake()
         {
             _generators = FindObjectsOfType<Generator>();
@@ -33,7 +36,7 @@ namespace TalosTest.Tool
             {
                 _segmentsbyGenerator.Add(connector, new List<List<LaserSegment>>());
             }
-            
+
             _laserPathGenerator = new LaserPathGenerator(obstacleMask);
         }
 
@@ -54,9 +57,9 @@ namespace TalosTest.Tool
                 connector.OnDrop -= OnPickUp;
             }
         }
+
         private void ResetAll()
         {
-            laserVFXController.Clear();
             foreach (var laser in _previousFrameInteractables)
             {
                 if (!_currentFrameInteractables.Contains(laser))
@@ -70,12 +73,21 @@ namespace TalosTest.Tool
             {
                 _previousFrameInteractables.Add(laser);
             }
-
-            laserVFXController.Clear();
+            
+            foreach (var kvp in _segmentsbyGenerator)
+            {
+                foreach (var list in kvp.Value)
+                {
+                    list.Clear();
+                }
+                
+                kvp.Value.Clear();
+            }
+            
             laserVFXController.Clear();
             _currentFrameInteractables.Clear();
         }
-        
+
         private void Update()
         {
             UpdateGeneratorLasers();
@@ -85,37 +97,59 @@ namespace TalosTest.Tool
         {
             UpdateGeneratorLasers();
         }
-
-        public Dictionary<Generator, List<List<LaserSegment>>> _segmentsbyGenerator;
         
         private void UpdateGeneratorLasers()
         {
             ResetAll();
 
-            foreach (var kvp in _segmentsbyGenerator)
-            {
-                kvp.Value.Clear();
-            }
-            
             foreach (var generator in _generators)
             {
                 var paths = _laserPathGenerator.FindAllPathSegments(generator);
+                _segmentsbyGenerator[generator] = paths;
+            }
 
+            var allSegments = new List<(Generator, LaserSegment)>();
+            foreach (var kvp in _segmentsbyGenerator)
+            {
+                var generator = kvp.Key;
+                foreach (var path in kvp.Value)
+                {
+                    foreach (var segment in path)
+                    {
+                        allSegments.Add((generator, segment));
+                    }
+                }
+            }
+
+            var blockedSegmentInfos = MathematicsHelper.GetLaserSegmentBlockers(allSegments);
+
+            foreach (var (generator, paths) in _segmentsbyGenerator)
+            {
                 foreach (var segments in paths)
                 {
                     foreach (var segment in segments)
                     {
                         var from = segment.From;
                         var to = segment.To;
+
+                        if (blockedSegmentInfos.TryGetValue(segment, out var blockInfo))
+                        {
+                            laserVFXController.DisplayLaserEffectConnection(generator.LaserColor, segment.From.LaserPoint, blockInfo.CollisionPoint);
+                            break;
+                        }
+
                         ConnectLaser(to, generator.LaserColor);
                         laserVFXController.DisplayLaserEffectConnection(generator.LaserColor, from.LaserPoint, to.LaserPoint);
                     }
                 }
-                
-                _segmentsbyGenerator.Add(generator, paths);
+            }
+
+            foreach (var info in blockedSegmentInfos.Values)
+            {
+                laserVFXController.DisplayHitMark(info.CollisionPoint);
             }
         }
-        
+
         private void ConnectLaser(LaserInteractable target, ColorType currentColor)
         {
             target.AddInputColor(currentColor);
@@ -126,14 +160,10 @@ namespace TalosTest.Tool
         private bool DebugCheckPathIteration(int index)
         {
             if (onlyPathTargetDebug > 0 && index != onlyPathTargetDebug - 1)
-            {
                 return true;
-            }
 
             if (maxPathCountDebug > 0 && index > maxPathCountDebug - 1)
-            {
                 return true;
-            }
 
             return false;
         }
---
+++ Assets/TalosTest/Scripts/Tool/MathematicsHelper.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TalosTest.Tool
+{
+    public static class MathematicsHelper
+    {
+        public static Dictionary<LaserSegment, BlockInfo> GetLaserSegmentBlockers(List<(Generator, LaserSegment)> allSegments)
+        {
+            Dictionary<LaserSegment, BlockInfo> blockedSegmentInfos = new();
+            for (var i = 0; i < allSegments.Count; i++)
+            {
+                for (var j = i + 1; j < allSegments.Count; j++)
+                {
+                    var (genA, segA) = allSegments[i];
+                    var (genB, segB) = allSegments[j];
+
+                    if (genA == genB)
+                    {
+                        continue;
+                    }
+
+                    if (CheckLasersIntersect(segA, segB))
+                    {
+                        var intersection = GetLaserIntersectionPoint(segA, segB);
+
+                        blockedSegmentInfos[segA] = new BlockInfo(intersection, segB);
+                        blockedSegmentInfos[segB] = new BlockInfo(intersection, segA);
+                    }
+                }
+            }
+
+            return blockedSegmentInfos;
+        }
+        
+        public static bool CheckLasersIntersect(LaserSegment a, LaserSegment b)
+        {
+            var a1 = a.From.LaserPoint;
+            var a2 = a.To.LaserPoint;
+            var b1 = b.From.LaserPoint;
+            var b2 = b.To.LaserPoint;
+
+            return LinesIntersect(a1, a2, b1, b2);
+        }
+
+        public static Vector3 GetLaserIntersectionPoint(LaserSegment firstSegment, LaserSegment secondSegment)
+        {
+            var p1 = firstSegment.From.LaserPoint;
+            var p2 = firstSegment.To.LaserPoint;
+            var q1 = secondSegment.From.LaserPoint;
+            var q2 = secondSegment.To.LaserPoint;
+
+            ClosestPointsOnTwoLines(out var c1, out var c2, p1, (p2 - p1).normalized, q1, (q2 - q1).normalized);
+            return (c1 + c2) * 0.5f;
+        }
+
+        public static bool ClosestPointsOnTwoLines(out Vector3 pointLine1, out Vector3 pointLine2,
+            Vector3 linePoint1, Vector3 lineVec1, Vector3 linePoint2, Vector3 lineVec2)
+        {
+            pointLine1 = Vector3.zero;
+            pointLine2 = Vector3.zero;
+
+            var a = Vector3.Dot(lineVec1, lineVec1);
+            var b = Vector3.Dot(lineVec1, lineVec2);
+            var e = Vector3.Dot(lineVec2, lineVec2);
+
+            var d = a * e - b * b;
+            if (Mathf.Abs(d) < 0.0001f)
+            {
+                return false;
+            }
+
+            var r = linePoint1 - linePoint2;
+            var c = Vector3.Dot(lineVec1, r);
+            var f = Vector3.Dot(lineVec2, r);
+
+            var s = (b * f - c * e) / d;
+            var t = (a * f - c * b) / d;
+
+            pointLine1 = linePoint1 + lineVec1 * s;
+            pointLine2 = linePoint2 + lineVec2 * t;
+
+            return true;
+        }
+
+        public static bool LinesIntersect(Vector3 p1, Vector3 p2, Vector3 q1, Vector3 q2)
+        {
+            var a = new Vector2(p1.x, p1.z);
+            var b = new Vector2(p2.x, p2.z);
+            var c = new Vector2(q1.x, q1.z);
+            var d = new Vector2(q2.x, q2.z);
+
+            return CheckLineSegmentsIntersect(a, b, c, d);
+        }
+
+        public static bool CheckLineSegmentsIntersect(Vector2 p, Vector2 p2, Vector2 q, Vector2 q2)
+        {
+            float o1 = Orientation(p, p2, q);
+            float o2 = Orientation(p, p2, q2);
+            float o3 = Orientation(q, q2, p);
+            float o4 = Orientation(q, q2, p2);
+
+            return !Mathf.Approximately(o1, o2) && !Mathf.Approximately(o3, o4);
+        }
+
+        public static int Orientation(Vector2 a, Vector2 b, Vector2 c)
+        {
+            var val = (b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y);
+            if (Mathf.Approximately(val, 0))
+            {
+                return 0;
+            }
+            
+            return val > 0 ? 1 : 2;
+        }
+    }
+}
